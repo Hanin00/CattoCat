c5262ec9b92eb623681e7002ab5e0bd9aed76b54
Hanin <haeun5419@gmail.com>
2021-09-11 15:58:34 +0900

    NaverMap - 현재 위치 띄움 / 2가지 방법

--- app/src/main/java/com/example/cattocat/src/main/map/MapActivity.kt
+++ app/src/main/java/com/example/cattocat/src/main/map/MapActivity.kt
@@ -1,12 +1,17 @@
 package com.example.cattocat.src.main.map
 
 import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.cattocat.databinding.ActivityMapBinding
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.widget.LocationButtonView
 
 
 //map
@@ -14,8 +19,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapBinding
     private lateinit var mapView: MapView
     private lateinit var naverMap: NaverMap
-    private var getLongitude: Double = 127.0246126
     private var getLatitude: Double = 37.5548732
+    private var getLongitude: Double = 127.0246126
+    private lateinit var locationSource: FusedLocationSource //사용자 현재 위치
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +32,40 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.onCreate(savedInstanceState)
 
         binding.mapNavermap.getMapAsync(this)
-
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
     }
 
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+        naverMap.let {
+            it.locationSource = locationSource
+            ActivityCompat.requestPermissions(this, PERMISSION, LOCATION_PERMISSION_REQUEST_CODE)
+            val uiSettings = it.uiSettings
+            uiSettings.isLocationButtonEnabled = true
+            uiSettings.isCompassEnabled = false
+            uiSettings.isScaleBarEnabled = false
+
+            val cameraPosition = CameraPosition(
+                LatLng(getLatitude, getLongitude),  // 위치 지정
+                9.0 // 줌 레벨
+            )
+/*
+            //지도 위 사용자 위치 표시 객체
+            val cameraUpdate = CameraUpdate.scrollTo(LatLng(getLatitude,getLongitude))
+            naverMap.moveCamera(cameraUpdate)
+            */
+
+            //카메라 위치 지정
+            naverMap.cameraPosition = cameraPosition
+            naverMap.locationSource = locationSource
+
+            val locationButton: LocationButtonView = binding.mapBtnLocation
+            locationButton.map = naverMap
+
+
+        }
+
+    }
 
 
     override fun onStart() {
@@ -67,35 +104,47 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
+    //현재위치 표시를 위한 권한
     private val PERMISSION: Array<String> = arrayOf(
         Manifest.permission.ACCESS_COARSE_LOCATION,
         Manifest.permission.ACCESS_FINE_LOCATION
     )
 
     //위치 권한
-    private val PERMISSION_REQUEST_CODE = 1000
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                grantResults[1] == PackageManager.PERMISSION_GRANTED
-            ) {
-                naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions,
+                grantResults
+            )
+        ) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                Log.d(TAG, "MainActivity - onRequestPermissionsResult 권한 거부됨")
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            } else {
+                Log.d(TAG, "MainActivity - onRequestPermissionsResult 권한 승인됨")
+                naverMap.locationTrackingMode = LocationTrackingMode.Follow // 현위치 버튼 컨트롤 활성
             }
+            return
         }
+
+        /*       if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+                   if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                       grantResults[1] == PackageManager.PERMISSION_GRANTED
+                   ) {
+                       naverMap.locationTrackingMode = LocationTrackingMode.Follow
+                   }
+               }*/
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
-        val cameraPosition = CameraPosition(
-            LatLng(33.38, 126.55),  // 위치 지정
-            9.0 // 줌 레벨
-        )
-        naverMap.cameraPosition = cameraPosition
-    }
 }
