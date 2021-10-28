feddee18de0f1bdb8a850a0f7f56348870431447
Hanin <haeun5419@gmail.com>
2021-10-28 13:14:47 +0900

    내고양이 화면 scroll 가능하게 수정 및 추가 버튼 생성

--- app/src/main/java/com/example/cattocat/src/addcat/AddCatService.kt
+++ app/src/main/java/com/example/cattocat/src/addcat/AddCatService.kt
@@ -11,25 +11,6 @@ class AddCatService (val view:AddCatView){
 
     fun tryPostAddCat(page: Int,mCallback: AddCatActivity) {
         val addCatRetrofitInterface = MyApplication.mRetrofit.create(AddCatRetrofitInterface::class.java)
-        addCatRetrofitInterface.postAddCat().enqueue(object : Callback<AddCatInfo> {
-            override fun onResponse( // 통신에 성공한 경우
-                call: Call<AddCatInfo>,
-                response: Response<AddCatInfo>
-            ) {
-                if(response.isSuccessful()){ // 응답을 잘 받은 경우
-                    mCallback.loadComplete(response.body()!!.data)
-                } else {
-                    // 통신은 성공했지만 응답에 문제가 있는 경우
-                    view.onPostAddCatFailure("다시 시도해주세요")
-                }
-            }
 
-            override fun onFailure(call: Call<AddCatInfo>, t: Throwable) {
-                // 통신에 실패한 경우
-               // view.onPostAddCatFailure("다시 시도해주세요")
-
-                Toast.makeText(this, "다시 시도해주세요",Toast.LENGTH_SHORT).show()
-            }
-        })
     }
 }
